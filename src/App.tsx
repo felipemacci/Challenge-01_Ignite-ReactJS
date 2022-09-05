2f2de4b60c5f8fae937b26793e114fb9c835b039
Felipe Macci <felipemacci@gmail.com>
2022-09-05 19:56:59 -0300

    creating post component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
+import { useState } from 'react'
 import { TaskCreatingBar } from './components/TaskCreatingBar'
-import { TasksContainer } from './components/TasksContainer'
+import { Task } from './components/Task'
 
 import './global.css'
 import styles from './App.module.css'
 import logo from './assets/todo.svg'
+import clipboard from './assets/clipboard.svg'
 
 export function App() {
+  const [tasks, setTask] = useState<string[]>([])
+  const [completedTasks, setCompletedTasks] = useState<string[]>([])
+
+  const addTask = (taskContent: string) => {
+    setTask([...tasks, taskContent])
+  }
+
+  const toggleTaskCheck = (task: string, isChecked: boolean) => {
+    if(isChecked) {
+      setCompletedTasks([...completedTasks, task])
+    } else {
+      const newTaskList = completedTasks.filter(taskContent => taskContent !== task)
+
+      setCompletedTasks(newTaskList)
+    }
+  }
+
+  const removeTask = (task: string) => {
+    const newTaskList = tasks.filter(taskContent => taskContent !== task)
+
+    setTask(newTaskList)
+  }
+
   return (
     <>
       <header className={ styles.header }>
@@ -13,8 +38,29 @@ export function App() {
       </header>
 
       <section className={ styles.tasks }>
-        <TaskCreatingBar />
-        <TasksContainer />
+        <TaskCreatingBar add={ addTask } />
+        
+        <div className={ styles.tasksContainer }>
+          <header className={ styles.taskCounters }>
+            <p>Created Tasks <span>{ tasks.length }</span></p>
+            <p>Completed <span>{ completedTasks.length } / { tasks.length }</span></p>
+          </header>
+
+          <main>
+            {
+              tasks.length > 0 ? (
+                tasks.map(text => {
+                  return <Task content={ text } toggleCheck={ toggleTaskCheck } remove={ removeTask } key={ text } />
+                })
+              ) : (
+                <div className={ styles.noTasks }>
+                  <img src={ clipboard } alt="clipboard" />
+                  <p><strong>You don't have any tasks registered yet</strong><br />Create tasks and organize your to-do items</p>
+                </div>
+              )
+            }
+          </main>
+        </div>
       </section>
     </>
   )
